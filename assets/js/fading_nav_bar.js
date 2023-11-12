51a4a4b7bbc3f02ab186e34c081ed05dd58c9cc0
RaymondFongHow <133295432+RaymondFongHow@users.noreply.github.com>
2023-11-12 23:17:20 +0800

    Add files via upload

---
+++ assets/js/fading_nav_bar.js
@@ -0,0 +1,21 @@
+console.log("Hii")
+console.log(nav)
+ 
+// window.addEventListener('scroll', () => {
+//     if (window.scrollY > 5) nav.style.background = 'black';
+//     else nav.style.background = 'white';
+// });
+
+
+// jQuery(function($) {
+//     $(window).scroll(function() {
+//         var scrollPos = $(window).scrollTop();
+//         var navOpacity = scrollPos / 800;
+
+//         $('nav').css("opacity", navOpacity);
+
+//         if ($('nav').css('opacity') < 0) {
+//             $('nav').css('opacity', 0)
+//         };
+//     });
+// });
